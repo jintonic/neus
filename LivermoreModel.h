c5b52a97a8ed83db25c3e5e0bf2fb4a912570577
Jing Liu <jing.leon@gmail.com>
2013-06-06 15:16:02 +0900

    added Lall and comments

--- LivermoreModel.h
+++ LivermoreModel.h
@@ -8,6 +8,13 @@ class TF2;
 
 namespace NEUS { class LivermoreModel; }
 
+/**
+ * C++ wrapper of Totani's interpolator Written in Fortran.
+ * TF1 and TF2 are used to visualize the results. TH1D and TH2D are created
+ * inside TF1 and TF2 for drawing. They are recreated if the scale of x axis is
+ * switched between linear and logarithmic for better visualization. They can
+ * also be used to get a value without going through the interpolation again.
+ */
 class NEUS::LivermoreModel : public SupernovaModel
 {
    protected:
@@ -31,35 +38,55 @@ class NEUS::LivermoreModel : public SupernovaModel
             const char *title="Livermore model");
       ~LivermoreModel();
 
+      /**
+       * Set path to directory holding wilson-*.dat
+       */
       void SetDataLocation(const char *dir);
+      /**
+       * Use <E> and N given in Divari 2012.
+       * Divari et al. claim that they use the Livermore model for their
+       * calculation in http://stacks.iop.org/0954-3899/39/i=9/a=095204. But
+       * the average energy and total number of neutrinos used in their paper
+       * are different from Totani's Fortran code. This function is used to set
+       * <E> and N to the values in their paper.
+       */
       void UseDivariData();
+      /**
+       * Clear Divari data.
+       */
       void Clear(Option_t *option="");
 
-      Double_t N2(UShort_t type, Double_t time, Double_t energy); // N(t, E) [1e50/s/MeV]
-      Double_t Ne(UShort_t type, Double_t energy); // N(t) [1e50/s]
-      Double_t Nt(UShort_t type, Double_t time); // N(E) [1e50/MeV]
-      Double_t Nall(UShort_t type); // N [1e50]
+      Double_t N2(UShort_t type, Double_t time, Double_t energy);
+      Double_t Ne(UShort_t type, Double_t energy);
+      Double_t Nt(UShort_t type, Double_t time);
+      Double_t Nall(UShort_t type);
+      Double_t Lall(UShort_t type);
       Double_t Eave(UShort_t type);
 
-      TH2D* HN2(UShort_t type=1); // N(t, E) [1e50/s/MeV]
-      TH1D* HNt(UShort_t type=1); // N(t) [1e50/s]
-      TH1D* HNe(UShort_t type=1, Double_t tmax=17.9012/*second*/); // N(E) [1e50/MeV]
+      TH2D* HN2(UShort_t type=1);
+      TH1D* HNt(UShort_t type=1);
+      /**
+       * Number of neutrinos integrated from [TMin(), tmax]
+       * If tmax>TMax(), tmax is set to be TMax().
+       * If tmax<10*second, tmax is set to be 10*second.
+       */
+      TH1D* HNe(UShort_t type=1, Double_t tmax=20);
 
-      TH2D* HL2(UShort_t type=1); // L(t, E) [1e50 erg/s/MeV]
-      TH1D* HLt(UShort_t type=1); // L(t) [1e50 erg/s]
-      TH1D* HLe(UShort_t type=1, Double_t tmax=17.9012/*second*/); // L(E) [1e50 erg/MeV]
+      TH2D* HL2(UShort_t type=1);
+      TH1D* HLt(UShort_t type=1);
+      TH1D* HLe(UShort_t type=1, Double_t tmax=20);
 
-      TH1D* HEt(UShort_t type=1); // <E>(t)/s
+      TH1D* HEt(UShort_t type=1);
 
-      TF2* FN2(UShort_t type=1); // N(t, E) [1e50/s/MeV]
-      TF1* FNt(UShort_t type=1); // N(t) [1e50/s]
-      TF1* FNe(UShort_t type=1, Double_t tmax=17.9012/*second*/); // N(E) [1e50/MeV]
+      TF2* FN2(UShort_t type=1);
+      TF1* FNt(UShort_t type=1);
+      TF1* FNe(UShort_t type=1, Double_t tmax=20);
 
-      TF2* FL2(UShort_t type=1); // L(t, E) [1e50 erg/s/MeV]
-      TF1* FLt(UShort_t type=1); // L(t) [1e50 erg/s]
-      TF1* FLe(UShort_t type=1, Double_t tmax=17.9012/*second*/); // L(E) [1e50 erg/MeV]
+      TF2* FL2(UShort_t type=1);
+      TF1* FLt(UShort_t type=1);
+      TF1* FLe(UShort_t type=1, Double_t tmax=20);
 
-      TF1* FEt(UShort_t type=1); // <E>(t)/s
+      TF1* FEt(UShort_t type=1);
 
       void Print();
 
